d80de9ff686d142f58b7dc8659ce2f22a581d978
Traineau <thomasraineau3@gmail.com>
2020-04-26 14:12:52 +0200

    Add: Header style

--- app/src/Header.elm
+++ app/src/Header.elm
@@ -1,55 +1,69 @@
 module Header exposing (..)
 
-import Html exposing (Html, a, button, div, text, p)
-import Html.Attributes exposing (class, href)
+import Html exposing (Html, a, button, div, text, p, img, ul, li, h1)
+import Html.Attributes exposing (class, href, src)
 
 -- MODEL
 
 type alias NavItem =
-  { name : String
-  , link : String
-  }
+    { name : String
+    , link : String
+    }
 
 type alias Model =
-  { itemsNav : List NavItem
-  }
+    { itemsNav : List NavItem
+    }
 
 init : Model
 init =
-  { itemsNav = 
-    [ { name = "Accueil"
-      , link = "/"
-      }
-    , { name = "A propos"
-      , link = "/about"
-      }
-    , { name = "Catégories"
-      , link = "/categories"
-      }
-    ]
-  }
+    { itemsNav = 
+        [ { name = "Accueil"
+        , link = "/"
+        }
+        , { name = "A propos"
+        , link = "/about"
+        }
+        , { name = "Catégories"
+        , link = "/categories"
+        }
+        ]
+    }
 
 -- UPDATE
 
 type Msg
-  = SetItemsNav (List NavItem)
+    = SetItemsNav (List NavItem)
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SetItemsNav items ->
-      { model | itemsNav = items }
+    case msg of
+        SetItemsNav items ->
+            { model | itemsNav = items }
 
 -- VIEW
 
 renderItem : NavItem -> Html msg
 renderItem item = 
-  a [ href item.link ] [ text item.name ]
+    li [class "nav-item"] [a [class "nav-link", href item.link ] [ text item.name ]]
+
+renderItems : List NavItem -> Html msg
+renderItems items = 
+    let 
+        navbar = List.map renderItem items
+    in
+        ul [class "main-nav" ] navbar
 
 view : Model -> Html msg
 view model =
-  let 
-    navbar =
-      List.map renderItem model.itemsNav
-    in
-      div [ ] navbar
+    div [class "header "]
+    [
+        div [class "container"]
+        [
+            div[class "nav-items"]
+            [
+                a[href "/"] [img [class "site-logo", src "./assets/gopher-orange.png"] [] ],
+                renderItems model.itemsNav,
+                h1[] [text "Gollery"]
+            ]
+        ]
+    ]
