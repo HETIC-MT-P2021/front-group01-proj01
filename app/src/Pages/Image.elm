b4ecf849522fdcd8b40e1005b959e99674264565
AlexandreLch <alexandre.lellouche@hetic.net>
2020-04-29 16:14:01 +0200

    Add: update form image

--- app/src/Pages/Image.elm
+++ app/src/Pages/Image.elm
@@ -1,7 +1,7 @@
 module Pages.Image exposing (view, Msg, update, init, Model)
 
-import Html exposing (Html, h1, map, text, div, span)
-import Html.Attributes exposing (class)
+import Html exposing (Html, h1, map, text, div, span, button, input, ul, li)
+import Html.Attributes exposing (class, type_, placeholder, value)
 import Html.Events exposing (..)
 import Http exposing (Error(..))
 import Json.Decode as Decode exposing (Decoder)
@@ -9,22 +9,37 @@ import Json.Decode as Decode exposing (Decoder)
 import Header
 import Footer
 
+type alias UpdateForm = 
+    { name : String
+    , desc : String
+    , tags : String
+    , categoryName : String
+    , id : Int
+    }
+
 init : Int -> ( Model, Cmd Msg )
 init id =
     ( { header = Header.init
       , footer = Footer.init
       , imageId = id
       , imageData = Nothing
+      , updateImageForm = 
+        { name = ""
+        , desc = ""
+        , tags = ""
+        , categoryName = ""
+        , id = id
+        }
       , error = Nothing
       }, Cmd.none )
 
 type alias Model =
-    { 
-        header: Header.Model
-        , footer: Footer.Model
-        , imageId: Int
-        , imageData: Maybe Image
-        , error: Maybe String
+    { header: Header.Model
+    , footer: Footer.Model
+    , imageId: Int
+    , imageData: Maybe Image
+    , error: Maybe String
+    , updateImageForm: UpdateForm
     }
 
 type alias Image =
@@ -43,6 +58,10 @@ type Msg
     | FooterMsg Footer.Msg
     | GetTheImage
     | GotItem (Result Http.Error (Image))
+    | UpdateCategory String
+    | UpdateName String
+    | UpdateDesc String
+    | UpdateTags String
 
 getImage : Model -> Cmd Msg
 getImage model = 
@@ -83,6 +102,38 @@ update msg model =
     GotItem (Err err) ->
         ( { model | error = Just <| errorToString err, imageData = Nothing }, Cmd.none )
 
+    UpdateCategory category ->
+        let
+            oldForm = model.updateImageForm
+            newForm = 
+                { oldForm | categoryName = category}
+        in
+        ({ model | updateImageForm = newForm }, Cmd.none)
+
+    UpdateName name ->
+        let
+            oldForm = model.updateImageForm
+            newForm = 
+                { oldForm | name = name}
+        in
+        ({ model | updateImageForm = newForm }, Cmd.none)
+    
+    UpdateDesc desc ->
+        let
+            oldForm = model.updateImageForm
+            newForm = 
+                { oldForm | desc = desc}
+        in
+        ({ model | updateImageForm = newForm }, Cmd.none)
+    
+    UpdateTags tags ->
+        let
+            oldForm = model.updateImageForm
+            newForm = 
+                { oldForm | tags = tags}
+        in
+        ({ model | updateImageForm = newForm }, Cmd.none)
+
 errorToString : Http.Error -> String
 errorToString error =
     case error of
@@ -114,5 +165,38 @@ view model =
                     span [] [text (String.fromInt model.imageId)]
                 ]
         ]
+        , div []
+                [text "Modifier l'image"]
+            , span []
+                [ div []
+                    [ text "Nom de l'image" ]
+                , viewInput "text" "Nom" model.updateImageForm.name UpdateName
+                , div [] [ text "Changer la description" ]
+                , viewInput "text" "Description" model.updateImageForm.desc UpdateDesc
+                , div [] [ text "Changer la catégorie" ]
+                , viewInput "text" "Catégorie" model.updateImageForm.categoryName UpdateCategory
+                , div [] [ text "Changer les tags" ]
+                , viewInput "text" "tag1,tag2,tag3..." model.updateImageForm.tags UpdateTags
+                , renderList (listTags model.updateImageForm.tags)
+                , button [] [ text "Modifier l'image" ]
+                ]
+        , div [] [ button [] [ text "Supprimer l'image" ] ]
         , map FooterMsg (Footer.view model.footer)
     ]
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+  
+listTags : String -> (List String)
+listTags str =
+  String.split "," str
+  
+renderList : List String -> Html msg
+renderList lst =
+    div []
+        [ text "Liste de tags"
+        , ul []
+            (List.map (\l -> li [] [ text l ]) lst)
+        ]
+    
